7897651818f3300607aae6d70e989827022268e1
Sadiq Ullah Khan <khansadiqullah006@gmail.com>
2026-08-31 05:32:34 +0500

    Implement the Save Lead functionality

---
+++ popup/popup.js
@@ -0,0 +1,59 @@
+const urlInput = document.querySelector("#url-input")
+const leadsList = document.querySelector("#leads-list")
+const saveInputBtn = document.querySelector("#save-btn")
+let leads = []
+
+// Fetch the leads from the localStorage
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("leads"))
+
+
+// If the localStorage is not empty
+if (leadsFromLocalStorage) {
+    // Render the leads
+    renderLeads(leadsFromLocalStorage)
+
+    leads = leadsFromLocalStorage
+}
+
+// Save Lead
+saveInputBtn.addEventListener("click", () => {
+    const url = urlInput.value
+
+    if (url === "") {
+        return
+    }
+
+    // If the lead is already in the list
+    if (leads.includes(url)) {
+        alert("This lead is already added to the list.")
+        return
+    }
+
+    // Save the lead to the leads array
+    leads.push(url)
+    
+
+    // Save the leads array to the localStorage
+    localStorage.setItem("leads", JSON.stringify(leads))
+
+    // Clear out the urlInput
+    urlInput.value = ""
+
+    // Render the leads
+    renderLeads(leads)
+})
+
+
+function renderLeads(leads) {
+    let leadsHTML = ""
+
+    leads.forEach(lead => {
+        leadsHTML += `
+           <li>
+                <a href="${lead}" target="_blank">${lead}</a>
+           </li>
+        `
+    })
+
+    leadsList.innerHTML = leadsHTML
+}
